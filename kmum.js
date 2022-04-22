9db9be496e44115e9c1cd0c71f9ed35063046e73
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2022-04-22 19:51:58 +0530

    Remove elasti

--- kmum.js
+++ kmum.js
@@ -6,19 +6,6 @@ const cognito_idp = new AWS.CognitoIdentityServiceProvider();
 
 exports.handler = async (event) => {
     debugger;
-    // You must always quit the redis client after it's used
-    redis.persist({
-        clusterIdentifier: 'kcluster',
-        params: ['K']
-    }, function (error, response, redisClient) {
-        if (error) {
-            console.log(error);
-            callback(error);
-        } else {
-            console.log(response);
-            redisClient.quit();
-        }
-    });
 
     return { "message": "Successfully executed" };
 };
