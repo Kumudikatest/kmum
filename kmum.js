a48fa19a9acd7478e79b03920f9c278abf800f0a
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2022-04-23 18:59:45 +0530

    Add SES resource

--- kmum.js
+++ kmum.js
@@ -2,10 +2,34 @@ const SL_REDIS = require('slappforge-sdk-redis');
 const clusterManager = require('./ClusterManager');
 const redis = new SL_REDIS.Redis(clusterManager);
 const AWS = require('aws-sdk');
+const ses = new AWS.SES();
 const cognito_idp = new AWS.CognitoIdentityServiceProvider();
 
 exports.handler = async (event) => {
-    debugger;
+
+    try {
+        let data = await ses.sendEmail({
+            Source: "testsigma18+April20th2022GSK@gmail.com",
+            Destination: {
+                ToAddresses: ['kumudika@adroitlogic.com'],
+                CcAddresses: ['kumudikarupasinghe@gmail.com']
+            },
+            Message: {
+                Subject: {
+                    Data: "KTest"
+                },
+                Body: {
+                    Text: {
+                        Data: `KTest description.`
+                    }
+                }
+            }
+        }).promise();
+        console.log(data);
+
+    } catch (err) {
+        console.log(err);
+    };
 
     return { "message": "Successfully executed" };
 };
